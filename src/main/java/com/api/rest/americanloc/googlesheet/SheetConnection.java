661b31458dea9c8f9a4e568c6e6c475b2b55360d
Stiven <stiven.giraldo@americaloc.com>
2021-10-25 11:30:53 -0500

    Se realizan ajustes en Sheet

--- src/main/java/com/api/rest/americanloc/googlesheet/SheetConnection.java
+++ src/main/java/com/api/rest/americanloc/googlesheet/SheetConnection.java
@@ -1,5 +1,14 @@
 package com.api.rest.americanloc.googlesheet;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
@@ -13,15 +22,6 @@ import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.BatchGetValuesResponse;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.security.GeneralSecurityException;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class SheetConnection {
 
 	private static final String APPLICATION_NAME = "Prueba";
@@ -33,7 +33,7 @@ public class SheetConnection {
 	 * these scopes, delete your previously saved tokens/ folder.
 	 */
 	private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS_READONLY);
-	private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+	//private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
 
 	/**
 	 * Creates an authorized Credential object.
@@ -44,10 +44,12 @@ public class SheetConnection {
 	 */
 	private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
 		// Load client secrets.
-		InputStream in = SheetConnection.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-		if (in == null) {
+		//InputStream in = SheetConnection.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+		String prueba = "{\"installed\":{\"client_id\":\"516656655672-obu53egbiugnqguailns4sgrpvp3aeea.apps.googleusercontent.com\",\"project_id\":\"proyectocreditoreal\",\"auth_uri\":\"https://accounts.google.com/o/oauth2/auth\",\"token_uri\":\"https://oauth2.googleapis.com/token\",\"auth_provider_x509_cert_url\":\"https://www.googleapis.com/oauth2/v1/certs\",\"client_secret\":\"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\",\"redirect_uris\":[\"urn:ietf:wg:oauth:2.0:oob\",\"http://localhost\"]}}";
+		InputStream in = new ByteArrayInputStream(prueba.getBytes());
+		/*if (in == null) {
 			throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
-		}
+		}*/
 		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
 		// Build flow and trigger user authorization request.
